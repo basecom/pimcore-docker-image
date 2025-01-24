e384057eb711145ccbf571c8d7b485f5c0d1afcc
Dennis Pfahlbusch <d.pfahlbusch@basecom.de>
2025-01-24 10:02:33 +0100

    (feat) update dependencies
    Signed-off-by: Dennis Pfahlbusch <d.pfahlbusch@basecom.de>

--- dist/images/php/8.0-fpm-pimcore/php/install_supercronic.sh
+++ dist/images/php/8.0-fpm-pimcore/php/install_supercronic.sh
@@ -4,14 +4,14 @@ set -e
 
 # Copied from https://github.com/aptible/supercronic/releases
 
-SUPERCRONIC_URL=https://github.com/aptible/supercronic/releases/download/v0.2.30/supercronic-linux-amd64
+SUPERCRONIC_URL=https://github.com/aptible/supercronic/releases/download/v0.2.33/supercronic-linux-amd64
 SUPERCRONIC=supercronic-linux-amd64
-SUPERCRONIC_SHA1SUM=9f27ad28c5c57cd133325b2a66bba69ba2235799
+SUPERCRONIC_SHA1SUM=71b0d58cc53f6bd72cf2f293e09e294b79c666d8
 
 if [ "$(uname -m)" = "aarch64" ]; then
-    SUPERCRONIC_URL=https://github.com/aptible/supercronic/releases/download/v0.2.30/supercronic-linux-arm64
+    SUPERCRONIC_URL=https://github.com/aptible/supercronic/releases/download/v0.2.33/supercronic-linux-arm64
     SUPERCRONIC=supercronic-linux-arm64
-    SUPERCRONIC_SHA1SUM=d5e02aa760b3d434bc7b991777aa89ef4a503e49
+    SUPERCRONIC_SHA1SUM=e0f0c06ebc5627e43b25475711e694450489ab00
 fi
 
 curl -fsSLO "$SUPERCRONIC_URL"
